687390c34a20c21d3a60952d5fb8cd9625d46028
scronkle <widdlyscudspizza@gmail.com>
2024-03-18 18:48:02 +1100

    add image thumbnails where needed

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,11 +3,28 @@ import styles from './Post.module.css'
 
 
 function Post (props) {
-  const {subreddit, thumbnail, permalink} = props.postData
+  const {subreddit, thumbnail, permalink, post_hint, is_self} = props.postData
+  let media = ""
+
+
+  //get the type of submission
+  if (is_self) {
+    media = "text"
+  } else if (post_hint) {
+    if (post_hint.includes('image')) {
+      media = "img"
+    } else if (post_hint.includes('video')) {
+      media = "video"
+    }
+  } else {
+    console.log('edge case:')
+    console.log(props.postData)
+  }
 
   return (
     <div className={styles.postCard} onClick={()=>{props.setPath(permalink)}}>
       <p>{`r/${subreddit}`}</p>
+      {media === "img" ? <img src={thumbnail}></img> : <p>text post</p>}
       <p className={styles.postTitle}>{props.postData.title}</p>
       <p className={styles.postText}>{props.postData.selftext}</p>
     </div>
