1fcbc03ff2a7655762154b21a1406951e4d530b0
scronkle <widdlyscudspizza@gmail.com>
2024-03-25 18:12:03 +1100

    add back button functionality (jesus christ)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,12 +12,16 @@ import { type } from '@testing-library/user-event/dist/type';
 
 function App() {
   const [path, setPath] = useState('best')
+  const [pathHistory, setPathHistory] = useState(['home'])
   const [feedPosts, setFeedPosts] = useState([])
   const [postInfo, setPostInfo] = useState({})
   const [view, setView] = useState('feed')
   
+
+  //updates display to show info from the new path
   function updateView() {
     Reddit.fetch(path).then((result)=>{
+      //check if current response is for a feed or a single post
       if (Array.isArray(result)) {
         setView('fullPost')
         setPostInfo(result)
@@ -28,18 +32,44 @@ function App() {
       console.log(view)
     })
   }
+
+  //adds last path to history (for when user navigates to new post/feed)
+  function updateHistory(newPath) {
+    console.log(`adding ${newPath} to:`)
+    console.log(pathHistory)
+    setPathHistory([newPath, ...pathHistory])
+  }
+
+  function navigate(newPath) {
+    console.log(`add ${path} to history`)
+    updateHistory(path)
+    setPath(newPath)
+  }
+
+  function navigateHistory(direction) {
+    if (direction === 'back') {
+      setPath(pathHistory[0])
+    }
+  }
   
-  
+  //when path changes, update view (and update history?)
   useEffect(()=>{
     updateView()
     console.log(path)
   }, [path])
 
+  useEffect(()=>{
+    console.log('current History')
+    console.log(pathHistory)
+  }, [pathHistory])
 
 
+
+
+  //adjust body display for feed/post view
   function bodyCheck() {
     if (view === 'feed') {
-      return <Feed posts={feedPosts} setPath={setPath}/>
+      return <Feed posts={feedPosts} setPath={navigate}/>
     } else {
       return <FullPost postInfo={postInfo}/>
     }
@@ -52,12 +82,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
+        <NavBar navigateHistory={navigateHistory}/>
       </header>
       <div className="body-container">
         <div className='side-panel'>
           <SearchBar/>
-          <Directory path={path} setPath={setPath}/>
+          <Directory path={path} setPath={navigate}/>
         </div>
         {bodyCheck()}
         <div className='side-panel'></div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,8 +5,14 @@ import { useState } from "react";
 
 function NavBar (props) {
 
+  function back() {
+    props.navigateHistory('back')
+  }
+
+
     return (
         <div className={styles.NavBar}>
+          <button className={styles.buttons} onClick={back}>back?</button>
           <p>
             miniReddit
           </p>
